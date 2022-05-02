58a5ac2f3c72164f3c12d041c4b5df36d3c48eae
chankaiqi <104771440+chankaiqi@users.noreply.github.com>
2022-05-02 22:20:31 +0800

    Update ex1-1.cpp

--- ex1-1.cpp
+++ ex1-1.cpp
@@ -1,9 +1,11 @@
-#include <stdio.h>
+#include <iostream>
+#include<stdio.h>
+using namespace std;
 
 int main()
 {
     printf("Hello 2\n");
-    cout<<"Hello World"
+    cout<<"Hello World";
 
     return 0;
 }
