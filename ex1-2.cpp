aedffad2b5677bf4097e07c61825c821f2d76775
chankaiqi <104771440+chankaiqi@users.noreply.github.com>
2022-05-02 23:00:56 +0800

    Update ex1-2.cpp

--- ex1-2.cpp
+++ ex1-2.cpp
@@ -2,5 +2,5 @@
 int main (void)
 {
      printf(" hello Selena\n"); 
-     printf(" my age is %d years old \n my heigh is %d m %d cm ",16,1,60);
+     printf(" my age is %d years old \n my height is %d m %d cm ",16,1,60);
 }
